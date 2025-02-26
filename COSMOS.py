f917d5973d499a2f4a5e76c43a6ec6ab13e3c3b3
lliu606 <lliu606@gatech.edu>
2025-02-25 22:59:51 -0500

    Update COSMOS.py

--- COSMOS.py
+++ COSMOS.py
@@ -21,7 +21,7 @@ def zeropower_via_newtonschulz5(G, steps=10, eps=1e-7):
     return X
 
 
-def soap_with_muon(params: List[Tensor],
+def soap_with_muon_gpt(params: List[Tensor],
             grads: List[Tensor],
             exp_avgs: List[Tensor],
             exp_avg_sqs: List[Tensor],
@@ -197,7 +197,7 @@ class COSMOS_for_gpt(Optimizer):
                     # record the step after step update
                     state_steps.append(state['step'])
 
-            soap_with_muon(
+            soap_with_muon_gpt(
                 params_with_grad,
                 grads,
                 exp_avgs,
@@ -216,5 +216,204 @@ class COSMOS_for_gpt(Optimizer):
                 maximize=group['maximize'],
                 ratio=self.lr_ratio,
                 )
+
+        return loss
+
+
+def soap_with_muon_llama(params: List[Tensor],
+          grads: List[Tensor],
+          exp_avgs: List[Tensor],
+          exp_avg_sqs: List[Tensor],
+          exp_avgs_GG: List[Tensor],
+          exp_avgs_P: List[Tensor],
+          max_exp_avg_sqs: List[Tensor],
+          state_steps: List[int],
+          *,
+          amsgrad: bool,
+          beta1: float,
+          beta2: float,
+          beta3: float,
+          lr: float,
+          weight_decay: float,
+          eps: float,
+          maximize: bool,
+          ratio: float,
+          ):
+
+    for i, param in enumerate(params):
+
+        grad = grads[i] if not maximize else -grads[i]
+        exp_avg = exp_avgs[i]
+        exp_avg_sq = exp_avg_sqs[i]
+        exp_avg_gg = exp_avgs_GG[i]
+        exp_avg_p = exp_avgs_P[i]
+
+        step = state_steps[i]
+
+        bias_correction1 = 1 - beta1 ** step
+        bias_correction2 = 1 - beta2 ** step
+        bias_correction3 = 1 - beta3 ** step
+
+        if len(param.size()) == 2 and param.size(0) <= 10000:
+            exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+
+            if step == 1:
+                W = torch.matmul(grad.T, grad)
+                U, _, _ = torch.linalg.svd(W, full_matrices=False)
+                exp_avg_p.data = U[:, :exp_avg_gg.size(0)]
+                exp_avg_gg = torch.matmul(torch.matmul(exp_avg_p.T, grad.T), torch.matmul(grad, exp_avg_p)) * (1 - beta3)
+
+            else:
+                t = exp_avg_p.detach().clone().T
+                exp_avg_p = beta3 * torch.matmul(exp_avg_p, exp_avg_gg) + (1 - beta3) * torch.matmul(grad.T,torch.matmul(grad,exp_avg_p))
+                exp_avg_p, _ = torch.linalg.qr(exp_avg_p, mode='reduced')
+                t = torch.matmul(t, exp_avg_p)
+                exp_avg_gg = beta3 * torch.matmul(t.T, torch.matmul(exp_avg_gg, t)) + \
+                             (1 - beta3) * torch.matmul(torch.matmul(grad, exp_avg_p).T, torch.matmul(grad, exp_avg_p))
+
+            scale = (grad.size(0) * grad.size(1)) ** 0.5
+            low_rank_grad = torch.matmul(grad, exp_avg_p)
+            exp_avg_sq.mul_(beta2).addcmul_(low_rank_grad, low_rank_grad.conj(), value=1 - beta2)
+
+            t = torch.matmul(exp_avg, exp_avg_p)
+            step_size = lr / bias_correction1
+            t1 = t / ((exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(eps)) * step_size
+            t1 = torch.matmul(t1, exp_avg_p.T)
+
+            t = exp_avg - torch.matmul(t, exp_avg_p.T)
+            t = zeropower_via_newtonschulz5(t, steps=5)
+            t = t / (t.norm() + eps)
+            t1.add_(t, alpha=scale * ratio * lr)
+            param.add_(t1 / (t1.norm() + eps), alpha=-scale * ratio * lr)
+
+        else:
+            exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+            exp_avg_sq.mul_(beta2).addcmul_(grad, grad.conj(), value=1 - beta2)
+
+            if amsgrad:
+                torch.maximum(max_exp_avg_sqs[i], exp_avg_sq, out=max_exp_avg_sqs[i])
+                denom = (max_exp_avg_sqs[i].sqrt() / math.sqrt(bias_correction2)).add_(eps)
+            else:
+                denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(eps)
+
+            step_size = lr / bias_correction1
+            param.addcdiv_(exp_avg, denom, value=-step_size)
+
+
+class COSMOS_for_llama(Optimizer):
+
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.98, 0.98), eps=1e-8, lr_ratio=0.1, rank=64,
+                 weight_decay=0, amsgrad=False, *, maximize: bool = False):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        if not 0.0 <= betas[2] < 1.0:
+            raise ValueError("Invalid beta parameter at index 2: {}".format(betas[2]))
+        if not 0.0 <= weight_decay:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay, amsgrad=amsgrad, maximize=maximize)
+        super(COSMOS_for_llama, self).__init__(params, defaults)
+        self.lr_ratio = lr_ratio
+        self.rank = rank
+
+    def __setstate__(self, state):
+        super(COSMOS_for_llama, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('amsgrad', False)
+            group.setdefault('maximize', False)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Args:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            params_with_grad = []
+            grads = []
+            exp_avgs = []
+            exp_avg_sqs = []
+            exp_avgs_GG = []
+            exp_avgs_P = []
+            max_exp_avg_sqs = []
+            state_steps = []
+            beta1, beta2, beta3 = group['betas']
+
+            for p in group['params']:
+                if p.grad is not None:
+                    params_with_grad.append(p)
+                    if p.grad.is_sparse:
+                        raise RuntimeError(
+                            'COSMOS does not support sparse gradients.')
+                    grads.append(p.grad)
+
+                    state = self.state[p]
+                    # Lazy state initialization
+                    if len(state) == 0:
+                        state['step'] = 0
+                        # Exponential moving average of gradient values
+                        state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                        # Exponential moving average of squared gradient values
+
+                        if len(p.size()) == 2 and p.size(0) <= 10000:
+                            state['exp_avg_GG'] = torch.zeros(self.rank, self.rank, dtype=p.data.dtype,
+                                                              device=p.data.device)
+                            state['exp_avg_P'] = torch.zeros(p.size(1), self.rank, dtype=p.data.dtype,
+                                                             device=p.data.device)
+                            state['exp_avg_sq'] = torch.zeros(p.size(0), self.rank, dtype=p.data.dtype,
+                                                              device=p.data.device)
+
+                        else:
+                            state['exp_avg_GG'] = torch.zeros(0)
+                            state['exp_avg_P'] = torch.zeros(0)
+                            state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                        if group['amsgrad']:
+                            # Maintains max of all exp. moving avg. of sq. grad. values
+                            state['max_exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                    exp_avgs.append(state['exp_avg'])
+                    exp_avg_sqs.append(state['exp_avg_sq'])
+                    exp_avgs_GG.append(state['exp_avg_GG'])
+                    exp_avgs_P.append(state['exp_avg_P'])
+
+                    if group['amsgrad']:
+                        max_exp_avg_sqs.append(state['max_exp_avg_sq'])
+
+                    state['step'] += 1
+                    state_steps.append(state['step'])
+
+            soap_with_muon_llama(
+                params_with_grad,
+                grads,
+                exp_avgs,
+                exp_avg_sqs,
+                exp_avgs_GG,
+                exp_avgs_P,
+                max_exp_avg_sqs,
+                state_steps,
+                amsgrad=group['amsgrad'],
+                beta1=beta1,
+                beta2=beta2,
+                beta3=beta3,
+                lr=group['lr'],
+                weight_decay=group['weight_decay'],
+                eps=group['eps'],
+                maximize=group['maximize'],
+                ratio=self.lr_ratio,
+            )
 
         return loss
